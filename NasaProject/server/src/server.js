4371eb8fb68df7712c345d54c46f089d5f79a5cf
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-08-05 00:18:13 +0300

    .

--- NasaProject/server/src/server.js
+++ NasaProject/server/src/server.js
@@ -11,8 +11,22 @@ const server = http.createServer(app);
 
 const MONGO_URL = 'mongodb+srv://yilmaz:azx09azx@nasacluster.llxwhwu.mongodb.net/?retryWrites=true&w=majority';
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready! ');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
+
 async function startServer() {
-  mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    // usefindAndModify: false,
+    //useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
   await loadPlanetsData(); // get the data from begining cause reading it is async. it may be not finish when we get request. we just make sure that we have data when we start server
 
   server.listen(PORT, () => {
